ad38d20ec91511b95aab67d1b23c29cfc4fb0e3d
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-07-01 21:10:16 -0400

    logo aspect

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,7 +1,11 @@
 const Header = () => (
   <header>
     <h1>
-      <img src="logo.svg" style={{ height: "1.2em" }} alt="" />
+      <img
+        src="logo.svg"
+        style={{ height: "1.2em", aspectRatio: "1 / 1" }}
+        alt=""
+      />
       SVG to PNG
     </h1>
   </header>
