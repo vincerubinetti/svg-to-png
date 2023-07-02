d6c28f59934c3e243e6c3ddaa6e92d93163e4796
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-07-02 00:05:50 -0400

    fix checkers bg

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/svg-to-png/" : "/",
+  base: "/svg-to-png/",
   plugins: [react()],
   resolve: {
     alias: {
